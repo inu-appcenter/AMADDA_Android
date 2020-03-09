ef15f26cbaae461ffba81e83e8801d46cf063631
ChoiAhHyun <cah1113@naver.com>
2020-03-09 19:35:28 +0900

    AddShareGroup, Setting, EditProfile, PushSetting Acitivities - toolbar

--- app/src/main/java/com/inu/amadda/view/activity/EditProfileActivity.java
+++ app/src/main/java/com/inu/amadda/view/activity/EditProfileActivity.java
@@ -1,9 +1,13 @@
 package com.inu.amadda.view.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.appcompat.widget.Toolbar;
 
 import com.inu.amadda.R;
 
@@ -13,6 +17,41 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+        setToolbar();
+
     }
+
+    private void setToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        AppCompatImageButton left_btn = toolbar.findViewById(R.id.toolbar_left_btn);
+        left_btn.setImageResource(R.drawable.cancel);
+        left_btn.setOnClickListener(onClickListener);
+
+        AppCompatImageButton right_btn_image = toolbar.findViewById(R.id.toolbar_right_image);
+        right_btn_image.setVisibility(View.GONE);
+
+        TextView right_btn_text = toolbar.findViewById(R.id.toolbar_right_text);
+        right_btn_text.setText("확인");
+        right_btn_text.setOnClickListener(onClickListener);
+
+        TextView title = toolbar.findViewById(R.id.toolbar_title);
+        title.setText("프로필 편집");
+    }
+
+    private View.OnClickListener onClickListener = view -> {
+        switch (view.getId()){
+            case R.id.toolbar_left_btn:{
+                onBackPressed();
+                break;
+            }
+            case R.id.toolbar_right_text:{
+
+                break;
+            }
+        }
+    };
 
 }
--- app/src/main/java/com/inu/amadda/view/activity/SettingActivity.java
+++ app/src/main/java/com/inu/amadda/view/activity/SettingActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.appcompat.widget.Toolbar;
 
 import com.inu.amadda.R;
 
@@ -16,8 +19,28 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        setToolbar();
         initialize();
 
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        AppCompatImageButton left_btn = toolbar.findViewById(R.id.toolbar_left_btn);
+        left_btn.setImageResource(R.drawable.cancel);
+        left_btn.setOnClickListener(onClickListener);
+
+        AppCompatImageButton right_btn_image = toolbar.findViewById(R.id.toolbar_right_image);
+        right_btn_image.setVisibility(View.GONE);
+
+        TextView right_btn_text = toolbar.findViewById(R.id.toolbar_right_text);
+        right_btn_text.setVisibility(View.GONE);
+
+        TextView title = toolbar.findViewById(R.id.toolbar_title);
+        title.setText("설정");
     }
 
     private void initialize() {
@@ -32,6 +55,10 @@ public class SettingActivity extends AppCompatActivity {
 
     private View.OnClickListener onClickListener = view -> {
         switch (view.getId()){
+            case R.id.toolbar_left_btn:{
+                onBackPressed();
+                break;
+            }
             case R.id.rl_profile:{
                 Intent intent = new Intent(this, EditProfileActivity.class);
                 startActivity(intent);
