78c659eb93a61946f14be12cd972624320248e49
ChoiAhHyun <cah1113@naver.com>
2020-05-06 23:54:19 +0900

    ManageInvitationActivity - 초대 온 개수 확인 TextView 작업 완료

--- app/src/main/java/com/inu/amadda/view/activity/ManageInvitationActivity.java
+++ app/src/main/java/com/inu/amadda/view/activity/ManageInvitationActivity.java
@@ -1,8 +1,11 @@
 package com.inu.amadda.view.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +13,32 @@ import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.Toolbar;
 
 import com.inu.amadda.R;
+import com.inu.amadda.etc.Constant;
+import com.inu.amadda.model.InvitationResponse;
+import com.inu.amadda.network.RetrofitInstance;
+import com.inu.amadda.util.PreferenceManager;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ManageInvitationActivity extends AppCompatActivity {
+
+    private String token;
+
+    private LinearLayout ll_blank;
+    private TextView tv_invitation_number, tv_message;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_invitation);
 
+        token = PreferenceManager.getInstance().getSharedPreference(getApplicationContext(), Constant.Preference.TOKEN, null);
+
         setToolbar();
+        initialize();
+        getInvitation();
 
     }
 
@@ -38,6 +59,56 @@ public class ManageInvitationActivity extends AppCompatActivity {
 
         TextView title = toolbar.findViewById(R.id.toolbar_title);
         title.setText("초대 관리");
+    }
+
+    private void initialize() {
+        ll_blank = findViewById(R.id.ll_blank);
+
+        tv_invitation_number = findViewById(R.id.tv_invitation_number);
+        tv_message = findViewById(R.id.tv_message);
+    }
+
+    private void getInvitation() {
+        RetrofitInstance.getInstance().getService().GetInvitations(token).enqueue(new Callback<InvitationResponse>() {
+            @Override
+            public void onResponse(Call<InvitationResponse> call, Response<InvitationResponse> response) {
+                int status = response.code();
+                if (response.isSuccessful()) {
+                    InvitationResponse invitationResponse = response.body();
+                    if (invitationResponse != null) {
+                        if (invitationResponse.success) {
+                            int num = invitationResponse.invitations.size();
+                            if (num > 0){
+                                ll_blank.setVisibility(View.INVISIBLE);
+                                tv_invitation_number.setVisibility(View.VISIBLE);
+                                tv_invitation_number.setText(num + "");
+                            }
+                            else {
+                                ll_blank.setVisibility(View.VISIBLE);
+                                tv_invitation_number.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "잠시 후 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                            Log.d("ManageInvitationActivity", invitationResponse.message);
+                        }
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "잠시 후 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "잠시 후 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                    Log.d("ManageInvitationActivity", status + "");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<InvitationResponse> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "인터넷 연결 상태를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                Log.d("ManageInvitationActivity", t.getMessage());
+            }
+        });
     }
 
     private View.OnClickListener onClickListener = view -> {
